976bf3f9e7e61bcb64e27728973b2e6a73e5a942
gubina_mariia <girlfrombaikal@gmail.com>
2022-05-17 21:24:51 +0300

    2.3.1 done

---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.UserDao;
+import web.model.User;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+
+    @Override
+    @Transactional
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(int id, String name, String surName, String email) {
+        userDao.updateUser(id, name, surName, email);
+    }
+
+    @Override
+    @Transactional
+    public void removeUser(int id) {
+        userDao.removeUser(id);
+    }
+
+    @Override
+    @Transactional
+    public User getUserById(int id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    @Transactional
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+}
